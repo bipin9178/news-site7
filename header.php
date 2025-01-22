7a519da3a4a005c4327f2bd2fb1c4885cfc09237
bipin9178 <bipinpkanzariya@gmail.com>
2025-01-22 09:43:56 +0530

    translate delect

--- header.php
+++ header.php
@@ -96,9 +96,6 @@ switch ($page) {
         </div>
         <!-- /LOGO -->
 
-        <div id="google_translate_element">
-
-        </div>
 
       </div>
   </div>
@@ -140,13 +137,3 @@ switch ($page) {
     </div>
   </div>
   <!-- /Menu Bar -->
-  <script type="text/javascript">
-    function googleTranslateElementInit() {
-      new google.translate.TranslateElement({
-        pageLanguage: 'en',
-        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-      }, 'google_translate_element');
-    }
-  </script>
-
-  <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
