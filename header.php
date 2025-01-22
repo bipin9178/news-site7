89b11c1f3a83e8f61165f171b0dc98e6889b4f06
bipin9178 <bipinpkanzariya@gmail.com>
2025-01-22 10:32:33 +0530

    change code

--- header.php
+++ header.php
@@ -71,7 +71,7 @@ switch ($page) {
     function googleTranslateElementInit() {
       new google.translate.TranslateElement({
         pageLanguage: 'en',
-        layout: google.translate.TranslateElement.InlineLayout.Horizonatal
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
       }, 'google_translate_element');
     }
   </script>
