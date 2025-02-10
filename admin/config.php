db34ac60a6ceb7210ccc0db025fc6703b6f680e4
bipin9178 <bipinpkanzariya@gmail.com>
2025-02-10 17:15:24 +0530

    chage file

--- admin/config.php
+++ admin/config.php
@@ -1,4 +1,4 @@
 <?php
 $hostname = "http://localhost/news-site";
 
-$conn = mysqli_connect("localhost", "root", "", "nres-site") or die("Connection failed : " . mysqli_connect_error());
+$conn = mysqli_connect("localhost", "root", "", "news-site") or die("Connection failed : " . mysqli_connect_error());
