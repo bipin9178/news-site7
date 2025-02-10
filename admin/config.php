b520e5c858ff6f1c17aa05d6c670d199f4a84aea
bipin9178 <bipinpkanzariya@gmail.com>
2025-02-10 16:45:24 +0530

    change

--- admin/config.php
+++ admin/config.php
@@ -1,6 +1,4 @@
 <?php
 $hostname = "http://localhost/news-site";
 
-$conn = mysqli_connect("localhost","root","","news-site") or die("Connection failed : " . mysqli_connect_error());
-
-?>
+$conn = mysqli_connect("sql211.infinityfree.com", "if0_38268194", "dYZpVsp4smgiGV ", "if0_38268194_economic1") or die("Connection failed : " . mysqli_connect_error());
